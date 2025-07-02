19a5d207bc3dd3539730757d1190837a19b53e7c
Sardor <safaraliyevsardor05@gmail.com>
2025-07-02 23:16:37 +0500

    commit

--- bot/product_file/products.py
+++ bot/product_file/products.py
@@ -3,17 +3,6 @@ from asgiref.sync import sync_to_async
 from apps.models import ProductColor
 
 
-def get_all_product_data():
-    products = ProductColor.objects.select_related('product').all()
-    return [
-        {
-            'id': pc.id,
-            'name': pc.product.name_uz,
-            'price': pc.price,
-        } for pc in products
-    ]
-
-
 @sync_to_async
 def get_products_by_category(category_id):
     return [
